1e29518b2be20ff310add583d784e5baab031336
JUAN <JUAN@JUAN-PC>
2016-03-05 20:54:58 +0100

    TAREA 4 ED

--- src/dam_ed04_actividad/Main.java
+++ src/dam_ed04_actividad/Main.java
@@ -4,21 +4,27 @@
  */
 
 package dam_ed04_actividad;
-
+/**
+ * 
+ * @author JUAN
+ */
 
 public class Main {
-
-    public static void main(String[] args) {
+	/**
+	 * variable
+	 */
+	private String jj;
+	public static void main(String[] args) {
         	operativa_cuenta(0);
     }
-
+    
 	private static void operativa_cuenta(float cantidad) {
 		CCuenta miCuenta1;
 		double saldoActual;
 		
 		miCuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
 		saldoActual = miCuenta1.estado();
-		System.out.println("El saldo actual es"+ saldoActual );
+		System.out.println("El saldo actual es "+ saldoActual );
 		
 		try {
 			miCuenta1.retirar(2300);
