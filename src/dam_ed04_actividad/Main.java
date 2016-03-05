61ea00a6b97362213843fddffd0b5ec2b98f5a6e
JUAN <JUAN@JUAN-PC>
2016-03-05 16:19:00 +0100

    TAREA 4 ED

---
+++ src/dam_ed04_actividad/Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dam_ed04_actividad;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        	operativa_cuenta(0);
+    }
+
+	private static void operativa_cuenta(float cantidad) {
+		CCuenta miCuenta1;
+		double saldoActual;
+		
+		miCuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+		saldoActual = miCuenta1.estado();
+		System.out.println("El saldo actual es"+ saldoActual );
+		
+		try {
+			miCuenta1.retirar(2300);
+		} catch (Exception e) {
+			System.out.print("Fallo al retirar");
+		}
+		try {
+			System.out.println("Ingreso en cuenta");
+			miCuenta1.ingresar(695);
+		} catch (Exception e) {
+			System.out.print("Fallo al ingresar");
+		}
+	}
+}
